711b0f9d4a1c8c284a62aea9a00ee3afe5296151
Kayzer <max.mlv2210@gmail.com>
2024-01-05 12:02:13 +0100

    Correction terminé

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-const changeColor = () => {
-  let number1 = Math.floor(Math.random() * 250);
-  let number2 = Math.floor(Math.random() * 250);
-  let number3 = Math.floor(Math.random() * 250);
+// Exercice :
 
-  let colorRGB = `rgb(${number1}, ${number2}, ${number3})`;
+// const changeColor = () => {
+//   let number1 = Math.floor(Math.random() * 250);
+//   let number2 = Math.floor(Math.random() * 250);
+//   let number3 = Math.floor(Math.random() * 250);
 
-  document.body.style.backgroundColor = colorRGB;
-  let containerHTML = `
-    <div> 
-      <p>${colorRGB}</p>
-    </div>
-  `;
-  document.body.innerHTML = containerHTML;
+//   let colorRGB = `rgb(${number1}, ${number2}, ${number3})`;
 
-  setTimeout(changeColor, 1000);
+//   document.body.style.backgroundColor = colorRGB;
+//   let containerHTML = `
+//     <div>
+//       <p>${colorRGB}</p>
+//     </div>
+//   `;
+//   document.body.innerHTML = containerHTML;
+
+//   setTimeout(changeColor, 1000);
+// };
+
+// changeColor();
+
+// *************************************************************************************************/
+
+// Correction
+
+const getColor = () => {
+  const r = Math.floor(Math.random() * 250);
+  const g = Math.floor(Math.random() * 250);
+  const b = Math.floor(Math.random() * 250);
+
+  const color = `rgb(${r},${g},${b})`;
+
+  document.body.style.background = color;
+  document.body.innerHTML = `<h2>${color}</h2>`;
 };
 
-changeColor();
+setInterval(getColor, 1000);
